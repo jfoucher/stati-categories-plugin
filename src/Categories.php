5f08d9fb9cf82542e3f472619bebea9f4dcc3b23
jfoucher <jfoucher@6px.eu>
2017-09-30 12:30:25 +0200

    Initial commit

---
+++ src/Categories.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Stati\Plugin\Categories;
+
+use Stati\Event\SiteEvent;
+use Stati\Event\SettingTemplateVarsEvent;
+use Stati\Plugin\Plugin;
+use Stati\Site\Site;
+use Stati\Site\SiteEvents;
+use Stati\Liquid\TemplateEvents;
+
+class Categories extends Plugin
+{
+    protected $name = 'categories';
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            SiteEvents::DID_READ_SITE => 'onAfterSiteRead',
+        );
+    }
+
+    public function onAfterSiteRead(SiteEvent $event)
+    {
+        $site = $event->getSite();
+
+        $posts = $site->getPosts();
+        $config = $site->getConfig();
+
+        $categories = [];
+        foreach ($posts as $post) {
+            if ($post->category) {
+                // post has a single category;
+                if (!isset($categories[$post->category])) {
+                    $categories[$post->category] = [$post];
+                } else {
+                    $categories[$post->category][] = $post;
+                }
+            }
+            if ($post->categories) {
+                $postCategories = $post->categories;
+                if (is_string($postCategories)) {
+                    $postCategories = explode(' ', $postCategories);
+                }
+                foreach ($postCategories as $category) {
+                    if (!isset($categories[$category])) {
+                        $categories[$category] = [$post];
+                    } else {
+                        $categories[$category][] = $post;
+                    }
+                }
+            }
+        }
+
+        $config['categories'] = $categories;
+        $site->setConfig($config);
+        return $categories;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
